2bed10c17ce1519c88f78810bf63ef5017ec4720
IgnacioGomez30 <126121927+IgnacioGomez30@users.noreply.github.com>
2024-10-14 21:05:37 -0300

    last commit

--- src/main/java/org/example/parcial_n1/repositories/AdnRepository.java
+++ src/main/java/org/example/parcial_n1/repositories/AdnRepository.java
@@ -1,14 +1,14 @@
-package org.example.parcial_n1.repositories;
+    package org.example.parcial_n1.repositories;
 
-import org.example.parcial_n1.entities.Adn;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+    import org.example.parcial_n1.entities.Adn;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-@Repository
-public interface AdnRepository extends JpaRepository<Adn, Long> {
-    Optional<Adn> findByAdn(String adnSequence);
+    @Repository
+    public interface AdnRepository extends JpaRepository<Adn, Long> {
+        Optional<Adn> findByAdn(String adnSequence);
 
-    long countByIsMutant(boolean isMutant);
-}
+        long countByIsMutant(boolean isMutant);
+    }
